149d3d6450e95e0a75091072b9ed03dfdc289c68
Calvin Cieslak <calvin.cieslak@gmail.com>
2020-02-11 08:32:05 -0800

    imp/remove-mount-point-test: Remove 'dummy test' comment

--- __tests__/redux-mount-point.js
+++ __tests__/redux-mount-point.js
@@ -2,7 +2,6 @@ import { NAME } from '../src/constants'
 
 export default NAME
 
-// dummy test
 describe('Redux mount point', () => {
   it('Should match NAME export in constants', () => {
     expect(NAME).toBe('waiter')
